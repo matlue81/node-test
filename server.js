8ac00d4e76772300eb8eb3b27390f41513b4441c
matlue81 <mathias.luestraeten@gmail.com>
2016-12-23 22:59:10 +0100

    revert last change

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 // Load the http module to create an http server.
-var express = require('express');
-var app = express();
+var http = require('http');
 var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
 
@@ -12,14 +11,4 @@ var server = http.createServer(function (request, response) {
 
 server.listen(server_port, server_ip_address, function () {
   console.log( "Listening on " + server_ip_address + ", port " + server_port )
-});
-
-
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
-
-app.listen(port, ipaddress, function() {
-        console.log('%s: Node server started on %s:%d ...',
-                        Date(Date.now() ), ipaddress, port);
 });
