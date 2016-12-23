8ba017a8d8db6ea285a7356e892a83757cbdc0ff
matlue81 <mathias.luestraeten@gmail.com>
2016-12-23 22:37:58 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
+// Load the http module to create an http server.
+var http = require('http');
 var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
- 
+
+// Configure our HTTP server to respond with Hello World to all requests.
+var server = http.createServer(function (request, response) {
+  response.writeHead(200, {"Content-Type": "text/plain"});
+  response.end("Hello World\n");
+});
+
 server.listen(server_port, server_ip_address, function () {
   console.log( "Listening on " + server_ip_address + ", port " + server_port )
 });
