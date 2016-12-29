b2b54f7875b5677c59547955a23a004dc1128a4c
matlue81 <mathias.luestraeten@gmail.com>
2016-12-29 13:48:44 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
-var express = require( 'express' );
+var http = require('http');
+var express = require('express');
 var app = express();
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 
-app.get( '/', function ( req, res ) {
-  res.send( 'Hello World!' );
-} );
+app.set('port', process.env.OPENSHIFT_NODEJS_PORT || 8080);
+app.set('ip', process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1');
 
-var server = app.listen( server_port, server_ip_address, function () {
-  var host = server.address().address;
-  var port = server.address().port;
-  console.log( 'Listening at http://%s:%s', host, port );
-} );
+http.createServer(app).listen(app.get('port'), app.get('ip'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
